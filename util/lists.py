f8320ed7bd16ce182dbeda988127ab55d94d58f6
Jeremy Leipzig <leipzigj@email.chop.edu>
2016-08-09 12:41:31 -0400

    use classes to avoid globals and pass teh chr

--- util/lists.py
+++ util/lists.py
@@ -9,22 +9,25 @@ def makedir(adir):
     if not os.path.exists(adir):
         shell("mkdir -p " + adir)
 
-def cmp(a,b):
-    if dlocs[a][0] == dlocs[b][0]:
-       if dlocs[a][1] > dlocs[b][1]:
-           return 1
-       else:
-           return -1
-    else:
-       if dlocs[a][0] > dlocs[b][0]:
-           return 1
-       else:
-           return -1
+class onechromosome(dlocs):
+    def __init__(self, dlocs):
+        self.dlocs = dlocs
+    def cmp(a,b):
+        if self.dlocs[a][0] == self.dlocs[b][0]:
+           if self.dlocs[a][1] > self.dlocs[b][1]:
+               return 1
+           else:
+               return -1
+        else:
+           if self.dlocs[a][0] > self.dlocs[b][0]:
+               return 1
+           else:
+               return -1
 
 def combine(inputf,outputf):
-    dlocs = dict()
     "combine indels in the individual lists into a single list"
     indels = dict()
+    dlocs = dict()
     for name in inputf:
         fin = open(name, "r")
         for line in fin.readlines():
@@ -49,11 +52,9 @@ def combine(inputf,outputf):
     with open(outputf, "w") as out:
         for chr in chrs:
             dlocs = indels[chr]
+            onechr = onechromosome(dlocs)
             ks = list(dlocs.keys())
             # ks.sort(key=lamda x:dlocs[x[0])
-            ks.sort(key=cmp_to_key(cmp))
+            ks.sort(key=cmp_to_key(onechr.cmp))
             for loc in ks:
                 out.write(chr + ':' + loc + "\n")
-    return dlocs
-
-dlocs = combine()
